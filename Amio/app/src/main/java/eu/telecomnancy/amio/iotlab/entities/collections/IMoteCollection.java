070e7b3da4d4970e46fc660c123b58f24c9319da
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-10-18 20:26:44 +0200

    Create custom mote collection

---
+++ Amio/app/src/main/java/eu/telecomnancy/amio/iotlab/entities/collections/IMoteCollection.java
@@ -0,0 +1,43 @@
+package eu.telecomnancy.amio.iotlab.entities.collections;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import eu.telecomnancy.amio.iotlab.dto.MoteDto;
+import eu.telecomnancy.amio.iotlab.entities.Mote;
+
+/**
+ * Defines a Mote collection
+ * @see Mote
+ */
+public interface IMoteCollection {
+
+    /**
+     * Given a DTO, add it if it is not stored, and merge its value with its associated Mote if
+     * one mote already exists with the same name
+     * @param dto DTO to be merged
+     */
+    void addAndMergeFromDto(MoteDto dto);
+
+    /**
+     * Given its name, retrieve the associated Mote
+     * @param name Name of the mote to be retrieved
+     * @return The associated mote or null if it is not found
+     */
+    @Nullable
+    Mote find(String name);
+
+    /**
+     * Whether or not the collection is holding any Mote
+     * @return true if the collection does not contains any Mote; false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Get all stored mote as a list
+     * @return A list of all stored motes
+     */
+    List<Mote> toList();
+
+}
