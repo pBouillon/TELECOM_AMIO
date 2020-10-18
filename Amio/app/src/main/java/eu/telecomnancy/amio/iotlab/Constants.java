1adfc3bfc28ac336237cd6d2dbfba5eaf82830f2
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-10-18 18:17:04 +0200

    Add IoTLab constants

---
+++ Amio/app/src/main/java/eu/telecomnancy/amio/iotlab/Constants.java
@@ -0,0 +1,15 @@
+package eu.telecomnancy.amio.iotlab;
+
+public final class Constants {
+
+    public static final class Urls {
+
+        /**
+         * Local API, with the loopback IP
+         * see: https://developer.android.com/studio/run/emulator-networking#networkaddresses
+         */
+        public final static String LOCAL = "http://10.0.2.2:25578/iotlab/rest/data/1";
+
+    }
+
+}
