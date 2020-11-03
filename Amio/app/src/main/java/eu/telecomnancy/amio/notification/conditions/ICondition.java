e2ee6b7f5405ee3036ca97d26f06c0da083ea203
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-03 20:28:35 +0100

    Create interfaces for the conditions and rules definition

---
+++ Amio/app/src/main/java/eu/telecomnancy/amio/notification/conditions/ICondition.java
@@ -0,0 +1,16 @@
+package eu.telecomnancy.amio.notification.conditions;
+
+/**
+ * Define an atomic condition to be verified by the rules
+ *
+ * @see eu.telecomnancy.amio.notification.rules.IRule
+ */
+public interface ICondition {
+
+    /**
+     * Evaluate the condition
+     * @return true if the condition is validated; false otherwise
+     */
+    boolean evaluate();
+
+}
