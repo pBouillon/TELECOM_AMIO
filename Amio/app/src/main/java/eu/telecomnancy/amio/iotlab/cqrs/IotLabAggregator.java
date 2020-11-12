81897a388c66fa27bc17136e76a8cb24cb8e2409
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-12 10:01:55 +0100

    Move PollingContext propagation from method parameters to the aggregator constructor

--- Amio/app/src/main/java/eu/telecomnancy/amio/iotlab/cqrs/IotLabAggregator.java
+++ Amio/app/src/main/java/eu/telecomnancy/amio/iotlab/cqrs/IotLabAggregator.java
@@ -30,35 +30,51 @@ import okhttp3.ResponseBody;
 public class IotLabAggregator {
 
     /**
-     * Android logging tag for this class
-     */
-    private static final String TAG = IotLabAggregator.class.getName();
-
-    /**
      * Inner HttpClient used for HTTP requests
      */
     private final OkHttpClient _httpClient = new OkHttpClient();
 
     /**
-     * (Pure) Generate the API route to fetch all data relative to the provided label
+     * Polling context, hold the Android context
+     */
+    private final PollingContext _context;
+
+    /**
+     * Create the aggregator from the polling context
+     *
+     * @param context Polling context
+     */
+    public IotLabAggregator(PollingContext context) {
+        _context = context;
+    }
+
+    /**
+     * Generate the API route to fetch all data relative to the provided label
+     *
      * @param label Type of data to be fetched
      * @return The associated API route
      */
-    private static String generateRouteForLabel(String label, PollingContext context) {
+    private String generateRouteForLabel(String label) {
+        String baseUri = _context.androidContext
+                .getResources()
+                .getString(R.string.iot_lab_adress);
+
         String baseUrl = PreferenceManager
-                .getDefaultSharedPreferences(context.androidContext)
-                .getString(context.androidContext.getResources().getString(R.string.iot_lab_adress), null);
+                .getDefaultSharedPreferences(_context.androidContext)
+                .getString(baseUri, null);
+
         return baseUrl + Constants.Urls.API + "/" + label + "/last";
     }
 
     /**
      * Handle the provided IQuery and execute it
+     *
      * @param query IQuery to handle
      * @return The MoteDtoCollection holding all requested data according to the provided query
      * @throws IOException If any issue happened with the HTTP call
      * @throws InvalidParameterException If the query can't be handled by the aggregator
      */
-    public MoteDtoCollection handle(IQuery query, PollingContext context)
+    public MoteDtoCollection handle(IQuery query)
             throws IOException, InvalidParameterException {
         // Flag to check whether or not this query is forged to request a specific value from all
         // motes
@@ -74,7 +90,7 @@ public class IotLabAggregator {
         GetMotesDataTypeQuery getMotesDataTypeQuery = (GetMotesDataTypeQuery) query;
 
         // Generate the associated route from the query's label
-        String associatedRoute = generateRouteForLabel(getMotesDataTypeQuery.label, context);
+        String associatedRoute = generateRouteForLabel(getMotesDataTypeQuery.label);
 
         // Return the fetched data from the route
         return fetchAndRetrieveMoteCollectionFromUrl(associatedRoute);
@@ -82,6 +98,7 @@ public class IotLabAggregator {
 
     /**
      * Performs the HTTP Call to retrieve a mote collection
+     *
      * @param queryUrl Url to be queried
      * @return A mote collection with its object mapped to the JSON response of the request
      * @throws IOException If any issue happened with the HTTP call
@@ -90,32 +107,26 @@ public class IotLabAggregator {
             throws IOException {
         // Build the HTTP Request based on the query
         MoteDtoCollection moteDtoCollection;
-        try {
-            Request request = new Request.Builder()
-                    .url(queryUrl)
-                    .build();
 
-            // Send the HTTP GET request to the API
-            Response response = _httpClient
-                    .newCall(request)
-                    .execute();
+        Request request = new Request.Builder()
+                .url(queryUrl)
+                .build();
 
-            // Retrieve the payload of the request
-            ResponseBody payload = response.body();
+        // Send the HTTP GET request to the API
+        Response response = _httpClient
+                .newCall(request)
+                .execute();
 
-            // If any data is fetched, deserialize it into the appropriate object
+        // Retrieve the payload of the request
+        ResponseBody payload = response.body();
+
+        // If any data is fetched, deserialize it into the appropriate object
 
 
-            moteDtoCollection = (payload != null)
-                    ? new Gson().fromJson(payload.string(), MoteDtoCollection.class)
-                    : new MoteDtoCollection();
-        } catch (JsonSyntaxException e) {
-            Log.e(TAG, "Unable to parse recieved data");
-            moteDtoCollection = new MoteDtoCollection();
-        } catch (IllegalArgumentException e) {
-            Log.e(TAG, "Malformed Url");
-            moteDtoCollection = new MoteDtoCollection();
-        }
+        moteDtoCollection = (payload != null)
+                ? new Gson().fromJson(payload.string(), MoteDtoCollection.class)
+                : new MoteDtoCollection();
+
         return moteDtoCollection;
     }
 
