33a5dc5416516a7f9a50f7376829a21dd7ea5999
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-12 10:53:27 +0100

    Add missing comment

--- Amio/app/src/main/java/eu/telecomnancy/amio/ui/settings/SettingsMainFragment.java
+++ Amio/app/src/main/java/eu/telecomnancy/amio/ui/settings/SettingsMainFragment.java
@@ -6,10 +6,14 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import eu.telecomnancy.amio.R;
 
+/**
+ * A fragment representing a field to be changed in the preferences
+ */
 public class SettingsMainFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
+
 }
